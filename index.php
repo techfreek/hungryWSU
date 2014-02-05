596766db64c9efb5c69654e72cbb2f59fda58cd5
Alex Bahm <alexgbahm@gmail.com>
2014-02-05 05:25:45 +0000

    I got the database connection to work, removed the hard coded content, and added a response if there are no results.

--- index.php
+++ index.php
@@ -54,38 +54,44 @@
 			<div id="results">
 
 			<?php  
-				$time = new DateTime::CreateFromFormat("Y-m-d H:i:s", "011-07-26 20:05:00");
-				$day = date( "w", $timestamp);
-				$hour= $time->format('H');
-				$min = $time->format('i');
+				//print "Here!\n";
+				$db_conn = parse_ini_file("../config.ini");
+				date_default_timezone_set('America/Los_Angeles');
+				$date = date('m/d/Y h:i:s a', time());				
+				//print_r($date);
 				
-				include('../config.php');
+				$day = date("w");
+				$hour= date("H");
+				$min = date("i");
+			
+				//print $db_conn[DBHOST];
+				
 
-				try {
-					$db = new PDO("mysql:host=$DBHOST;dbname=$DBNAME", $DBUSER, $DBPASS);
+				try 
+				{
+					$db = new PDO("mysql:host=$db_conn[DBHOST];dbname=$db_conn[DBNAME]", $db_conn[DBUSER], $db_conn[DBPASS]);
 				}
-					catch(PDOException $e)
+				catch(PDOException $e)
+				{
 					echo $e->getMessage();
-				}			
-
-				$oldQuery = 'SELECT * FROM location;';
+				}
 				
-				$otherQuery = 'SELECT * FROM location INNER JOIN times ON location.id = times.location;';
-				
-				$query = 'SELECT * 
+				$query = "SELECT * 
 								FROM location
 								INNER JOIN times
 								ON location.id = times.location
-								WHERE day = $day
-								AND openHour > $hour
-								OR (openHour == $hour AND openMin <= $min);';
+								WHERE day =$day
+									AND ((openHour < $hour) OR ((openHour = $hour) AND (openMin >= $min)))";
 										
-				foreach ($db->query($query) as $restaurant)
+				$results = $db->query($query);
+
+				foreach ($results as $restaurant)
 				{
 					echo '<div class="result">';
-					echo '<div id="summary" class="pure-g">';
-					echo '<div class="pure-u-1-3"><div id="name"><a href="' . $restaurant['url'] . '">' . $restaurant['name'] . '</a></div></div>';
-					if( $restaurant['deliver'] == true)
+					echo '<div class="summary">';
+					echo '<div class="pure-g">';
+					echo '<div class="pure-u-1-3"><div id="name"><a href="' . $restaurant['website'] . '">' . $restaurant['name'] . '</a></div></div>';
+					if( $restaurant['deliver'] == 1)
 					{
 						echo '<div class="pure-u-1-3"><div class="yes">We\'ll deliver!</div></div>';
 					}
@@ -93,102 +99,31 @@
 					{
 						echo '<div class="pure-u-1-3"><div class="no">We don\'t deliver :(</div></div>';
 					}
-					echo '<div class="pure-u-1-3"><div id="' . $restaurant['category'] . '">' . $restaurant['category'] . '</div</div>';
+					echo '<div class="pure-u-1-3"><div id="type">' . $restaurant['category'] . '</div></div>';
+					echo '</div>';
 					echo '</div>';
 					echo '<div class="details">';
 					echo '<div class="pure-g">';
-					echo '<div class="pure-u-1-2"><div id="address" id="detail><a href="http://maps.google.com/maps?ie=UTF-8&amp;hl=en&amp;q=' . str_replace(' ', '+', $restaurant['street']) . '%0A' . $restaurant['city'] . '%2C+' . $restaurant['state'] . '+' . $restaurant['zip'] .'">' . $restaurant['street'] . ', ' . $restaurant['city'] . ', ' . $restaurant['state'] . ' ' . $restaurant['zip'] . '</a></div></div>';
-					echo '<div class="pure-u-1-2"><div id="phoneNum">Phone: ' . $restaurant['phone'] . '</div></div>';
-					echo '<div class="pure-u-1-2"><div id="openTill" openHour="' . $restaurant['openHour'] . '" openMinute="' . $restaurant['openMinute'] . '" closeHour="' . $restaurant['closeHour'] . '" closeMinute="' . $restaurant['closeMinute'] . '"></div></div>';
+					echo '<div class="pure-u-1-2"><div id="address" id="detail"><a href="http://maps.google.com/maps?ie=UTF-8&amp;hl=en&amp;q=' . str_replace(' ', '+', $restaurant['address']) . '%0A' . $restaurant['city'] . '%2C+' . $restaurant['state'] . '+' . $restaurant['zip'] .'">' . $restaurant['address'] . ', ' . $restaurant['city'] . ', ' . $restaurant['state'] . ' ' . $restaurant['zip'] . '</a></div></div>';
+					echo '<div class="pure-u-1-2"><div id="phoneNum">Phone: ' . $restaurant['phoneNumber'] . '</div></div>';
+					echo '<div class="pure-u-1"><div id="openTill" openHour="' . $restaurant['openHour'] . '" openMin="' . $restaurant['openMin'] . '" closeHour="' . $restaurant['closeHour'] . '" closeMinute="' . $restaurant['closeMin'] . '"></div></div>';
+					echo '</div>';
+					echo '</div>';
+					echo '</div>';
+				}
+				if(count($results) == 0)
+				{
+					echo '<div class="result">';
+					echo '<div class="summary">';
+					echo '<div class="pure-g">';
+					echo '<div class="pure-u-1">';					
+					echo 'div id="noResults">I guess nothing is open in Pullman..."</div>';
+					echo '</div>';
 					echo '</div>';
 					echo '</div>';
 					echo '</div>';
 				}
 			?>
-			
-			
-				<div class="result">
-					<!--Add timestamp that displays today's closing time-->
-					<div class="summary">
-						<div class="pure-g">
-							<div class="pure-u-1-3"><div id="name"><a href="http://www.redrobin.com/">Red Robin</a></div></div>
-							<div class="pure-u-1-3"><div class="yes">We'll deliver!</div></div>
-							<div class="pure-u-1-3"><div id="type">Food</div></div>
-						</div>
-					</div>
-					<div class="details">
-						<div class="pure-g">
-							<div class="pure-u-1-2"><div id="address" id="detail"><a href="http://maps.google.com/maps?ie=UTF-8&amp;hl=en&amp;q=1525+NE+Merman+Drive%0APullman%2C+WA+98121">123 ABC Street, Seattle, WA 98121	</a></div></div>
-							<div class="pure-u-1-2"><div id="phoneNum">Phone: (425) 417-5393</div></div>					
-							<div class="pure-u-1"><div id="openTill" openHour="8" openMinute="00" closeHour="2" closeMinute="30"></div></div>
-						</div>
-					</div>
-				</div>
-				<div class="result">
-					<!--Add timestamp that displays today's closing time-->
-					<div class="summary">
-						<div class="pure-g">
-							<div class="pure-u-1-3"><div id="name"><a href="http://www.zzubarandgrill.com/">The Zzu</a></div></div>
-							<div class="pure-u-1-3"><div class="yes">We'll deliver!</div></div>
-							<div class="pure-u-1-3"><div id="type">Food</div></div>
-						</div>
-					</div>
-					<div class="details">
-						<div class="pure-g">
-							<div class="pure-u-1-2"><div id="address" id="detail">453 Merman St, Pullman, WA 98121</div></div>
-							<div class="pure-u-1-2"><div id="phoneNum">Phone: (425) 345-2342</div></div>					
-							<div class="pure-u-1"><div id="openTill" openHour="12" openMinute="00" closeHour="3" closeMinute="30"></div></div>
-						</div>
-					</div>
-				</div>
-				<div class="result">
-					<div class="summary">
-						<div class="pure-g">
-							<div class="pure-u-1-3"><div id="name">Jimmy Johns</div></div>
-							<div class="pure-u-1-3"><div class="no">We don't deliver</div></div>
-							<div class="pure-u-1-3"><div id="type">Food</div></div>
-						</div>
-					</div>
-					<div class="details">
-						<div class="pure-g">
-							<div class="pure-u-1-2"><div id="address" id="detail">1920 Fail Dr, Failcity	, WA 99163</div></div>
-							<div class="pure-u-1-2"><div id="phoneNum">Phone: (425) 234-4347</div></div>
-							<div class="pure-u-1"><div id="openTill" openHour="8" openMinute="00" closeHour="22" closeMinute="30"></div></div>
-						</div>
-					</div>
-				</div>
-				<div class="result">
-					<div class="summary">
-						<div class="pure-g">
-							<div class="pure-u-1-3"><div id="name">McDonald's</div></div>
-							<div class="pure-u-1-3"><div class="no">We don't deliver</div></div>
-							<div class="pure-u-1-3"><div id="type">Food</div></div>
-						</div>
-					</div>
-					<div class="details">
-						<div class="pure-g">
-							<div class="pure-u-1-2"><div id="address" id="detail">3392 Stadium Way Pullman, WA 99163</div></div>
-							<div class="pure-u-1-2"><div id="phoneNum">Phone: (425) 674-7534</div></div>
-							<div class="pure-u-1"><div id="openTill" id="openTill" openHour="8" openMinute="00" closeHour="24" closeMinute="30"></div></div>
-						</div>
-					</div>
-				</div>
-				<div class="result">
-					<div class="summary">
-						<div class="pure-g">
-							<div class="pure-u-1-3"><div id="name">Pizza Hut</div></div>
-							<div class="pure-u-1-3"><div class="yes">We deliver</div></div>
-							<div class="pure-u-1-3"><div id="type">Food</div></div>
-						</div>
-					</div>
-					<div class="details">
-						<div class="pure-g">
-							<div class="pure-u-1-2"><div id="address" id="detail">3392 Main St Pullman, WA 99163</div></div>
-							<div class="pure-u-1-2"><div id="phoneNum">Phone: (425) 564-2343</div></div>
-							<div class="pure-u-1"><div id="openTill" openHour="1" openMinute="00" closeHour="23" closeMinute="00"></div></div>
-						</div>
-					</div>
-				</div>
 		</div>
 		
 	<?php include 'footer.php'; ?>	
