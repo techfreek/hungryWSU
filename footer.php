14dd386adaec502e8ebe8450d9bf89c3d63ac8ad
Alex Bahm <alexgbahm@gmail.com>
2014-02-10 08:18:51 +0000

    Tweaked the footer to have an additional disclaimer.

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
 <div id="footer">
                 <div class="pure-g">
-                        <div class="pure-u-1">Created by Alex Bahm and Connor Jackson</div>
-                        <div class="pure-u-1"><div id="affil">DrunkWSU.com has no affiliation with WSU</div></div>
-			<div class="pure-u-1">Represents normal school year hours and may change without notice.</div>
+                        <div class="pure-u-1"><div id="disclaimer">DrunkWSU.com has no affiliation with WSU</div></div>
+			<div class="pure-u-1"><div id="disclaimer"><div id="affil">Represents normal school year hours and may change without notice.</div></div></div>
+			<div class="pure-u-1"><div id="disclaimer">Please drink responsibly. Don't drink and drive.</div></div>
                         <div class="pure-u-1-3" id="left"><a href="mailto:drunkwsu@gmail.com?Subject=Web%20Master">Contact the Webmaster</a></div>
                         <div class="pure-u-1-3"><div id="policy"><a href="http://www.iubenda.com/privacy-policy/359390" class="iubenda-white iubenda-embed" title="Privacy Policy">Privacy Policy</a><script type="text/javascript">(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src = "http://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);</script></div></div>
 						<div class="pure-u-1-3" id="right"><a href="mailto:drunkwsu@gmail.com?Subject=Promotion%20Request">Promote Your Business</a></div>
