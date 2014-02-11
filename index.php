f6c953b6b32c60fb5b50869b3913b595675b8c7d
Alex Bahm <alexgbahm@gmail.com>
2014-02-11 18:20:30 +0000

    The site is now live! Turns out this server is running at about 100% capacity when a lot of people get on imagine that. I just added google analytics so I can get an idea of who vists the site.

--- index.php
+++ index.php
@@ -22,6 +22,21 @@
   ?>
 </head>
 <body>
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-47962628-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
 <?php
 	require_once 'Mobile_Detect.php';
 	$detect = new Mobile_Detect;
